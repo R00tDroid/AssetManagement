3e39cccef933a826d7277fe5b47e1c06847e8f3c
RootDroid <RootDroid@4a879e22-a77c-4755-96dc-732b041d4d15>
2020-01-04 16:00:59 +0000

    Add asset action tooltip data
    git-svn-id: svn://89.35.76.3/AssetManagement@11 4a879e22-a77c-4755-96dc-732b041d4d15

--- Plugins/AssetManagement/Source/AssetManagement/Private/Actions/AssetActionUnusedCheck.h
+++ Plugins/AssetManagement/Source/AssetManagement/Private/Actions/AssetActionUnusedCheck.h
@@ -6,4 +6,6 @@ class AssetActionUnusedCheck : public IAssetAction
 public:
 	void ScanAssets(TArray<FAssetInfo>& Assets, uint16 AssignedId) override;
 	void ExecuteAction(TArray<FAssetData> Assets) override;
+	FString GetTooltipHeading() override { return "Unused Asset"; }
+	FString GetTooltipContent() override { return "This asset is not used by a playable level.\n\nClick to delete"; }
 };
